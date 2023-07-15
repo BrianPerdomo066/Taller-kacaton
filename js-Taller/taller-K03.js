01cf5e722fcb520740cb3a8c6e6b37489033e7da
brian perdomo <brayanperdomovelasquez@gmail.com>
2023-07-14 19:23:52 -0500

    [+]agrege el el punto (03, 02, 04)

---
+++ js-Taller/taller-K03.js
@@ -0,0 +1,10 @@
+// Escribe una función que reciba una cadena de 
+// texto con varias palabras separadas por espacios y devuelva la 
+// cadena de texto con las palabras ordenadas 
+// alfabéticamente.
+
+
+const nombres = ["axel","cristian","jhon","daniel","gustabo","luis", "brian"];
+console.log( nombres );
+console.log( nombres.sort());
+
---
+++ js-Taller/taller-k02.js
@@ -0,0 +1,17 @@
+// Escribe una función que reciba un array de números 
+// ordenados de forma ascendente
+// y devuelva el o los números faltantes en la secuencia.
+
+
+const n = [1, 6, 15];
+
+const x = [];
+
+for (let i = 1;i <= 15; i++) {
+    x.push( i )
+}
+
+console.log ( x );
+
+
+
